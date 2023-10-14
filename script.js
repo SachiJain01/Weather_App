7d6dc6daac334a2ffc58dc59f8d6b880a822f174
sjain63 <sachi.a.jain@aricent.com>
2023-10-14 19:49:51 +0530

    FirstCommit

---
+++ script.js
@@ -0,0 +1,15 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
